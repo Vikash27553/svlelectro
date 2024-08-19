c5aa24777c63a4378ed01e3108323c57a9aad920
VIKASH KAUSHIK <113708343+Vikash27553@users.noreply.github.com>
2024-08-19 15:35:51 +0530

    Add files via upload
    add files

---
+++ java_script.js
@@ -0,0 +1,21 @@
+let menulist = document.getElementById("menulist");
+    menulist.style.height= "8vh";
+  
+
+
+
+    function  togglemenu(){
+
+      if( menulist.style.height == "8vh")
+        {
+
+         menulist.style.height= "100vh";
+
+      }
+
+      else {
+        menulist.style.height= "8vh";
+
+      }
+    }
+     
